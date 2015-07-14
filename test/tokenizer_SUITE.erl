5246af4f6f792975e78df3907b6df35638953d06
Wojciech Gawronski <afronski@gmail.com>
2015-07-14 19:16:12 +0200

    Additional test cases.

--- test/tokenizer_SUITE.erl
+++ test/tokenizer_SUITE.erl
@@ -6,11 +6,12 @@
 -include("../include/tokens_definitions.hrl").
 
 -export([ all/0 ]).
--export([ tokenize_input/1, clean_input/1,
-          accepting_tokens_from_other_sets/1
-        ]).
+-export([ tokenize_input/1, tokenization_edge_cases/1,
+          clean_input/1,
+          accepting_tokens_from_other_sets/1 ]).
 
-all() -> [ tokenize_input, clean_input,
+all() -> [ tokenize_input, tokenization_edge_cases,
+           clean_input,
            accepting_tokens_from_other_sets ].
 
 tokenize_input(_Context) ->
@@ -19,8 +20,13 @@ tokenize_input(_Context) ->
     ?assertEqual(["[", "]"], bferl_tokenizer:from_string("[]")),
     ?assertEqual(["<", ">"], bferl_tokenizer:from_string("<>")).
 
+tokenization_edge_cases(_Context) ->
+    ?assertEqual([], bferl_tokenizer:from_string("\tqwertyuiop{}\\|asdfghjkl:;\"'zxcvbnm?/\n\r1234567890!@#$%^&*()`~_=")),
+    ?assertEqual([], bferl_tokenizer:from_string("")).
+
 clean_input(_Context) ->
     ?assertEqual([".", ",", "[", "]", "<", ">", "+", "-"], bferl_tokenizer:from_string("\tABCD .,[]<>+- ABCD\t")).
 
 accepting_tokens_from_other_sets(_Context) ->
-    ?assertEqual([".", ",", "[", "]", "<", ">", "+", "-", "Y"], bferl_tokenizer:from_string("\tABCD .,[]<>+-Y ABCD\t", ?BRAINFORK)).
+    ?assertEqual([".", ",", "[", "]", "<", ">", "+", "-", "Y"], bferl_tokenizer:from_string("\tABCD .,[]<>+-Y ABCD\t", ?BRAINFORK)),
+    ?assertEqual(["Y"], bferl_tokenizer:from_string("XYZ", ?BRAINFORK)).
