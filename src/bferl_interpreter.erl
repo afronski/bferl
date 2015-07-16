30ed98267ef4b668c4b5f915816ed74398441c70
Wojciech Gawronski <afronski@gmail.com>
2015-07-16 20:23:37 +0200

    Working interpreter skeleton.

--- src/bferl_interpreter.erl
+++ src/bferl_interpreter.erl
@@ -2,7 +2,48 @@
 
 -include("../include/interpreter_definitions.hrl").
 
--export([ init/0 ]).
+-export([ init/0, init/1,
+          load/2, get_memory_cell/2,
+          step/1, run/1 ]).
 
 init() ->
     #interpreter_state{}.
+
+init(Program) ->
+    load(Program, #interpreter_state{}).
+
+load(Program, State) when is_list(Program) ->
+    State#interpreter_state{instructions = Program}.
+
+get_memory_cell(CellIndex, State) ->
+    array:get(CellIndex, State#interpreter_state.memory).
+
+step(State) when State#interpreter_state.instructions =:= undefined ->
+    no_program_loaded;
+
+step(State) when State#interpreter_state.instructions_pointer < 0 ->
+    end_of_program;
+
+step(State) when State#interpreter_state.instructions_pointer >= length(State#interpreter_state.instructions) ->
+    end_of_program;
+
+step(State) ->
+    IP = State#interpreter_state.instructions_pointer,
+    Instruction = lists:nth(IP + 1, State#interpreter_state.instructions),
+    TemporaryState = do(Instruction, State),
+    TemporaryState#interpreter_state{instructions_pointer = IP + 1}.
+
+run(State) ->
+    IP = State#interpreter_state.instructions_pointer,
+    Program = State#interpreter_state.instructions,
+    SubProgram = lists:sublist(Program, IP + 1, length(Program)),
+    lists:foldl(fun (_, PartialState) -> step(PartialState) end, State, SubProgram).
+
+%% Opcodes
+%% --
+%% Brainfuck
+
+do("+", InputState) ->
+    CellIndex = InputState#interpreter_state.memory_pointer,
+    Cell = get_memory_cell(CellIndex, InputState),
+    InputState#interpreter_state{memory = array:set(CellIndex, Cell + 1, InputState#interpreter_state.memory)}.
--- test/interpreter_SUITE.erl
+++ test/interpreter_SUITE.erl
@@ -5,12 +5,24 @@
 
 -include("../include/interpreter_definitions.hrl").
 
+-define(TO_STRING(Code), string:join(Code, "")).
+
 -export([ all/0 ]).
 -export([ empty_state_should_have_fixed_memory_size/1,
-          pointers_should_be_set_at_the_beginning_after_init/1 ]).
+          pointers_should_be_set_at_the_beginning_after_init/1,
+          after_loading_program_it_should_be_available_in_state/1,
+          you_should_be_able_run_program/1,
+          you_should_be_able_step_through_program/1,
+          stepping_out_of_the_program_should_not_be_a_problem/1,
+          running_partially_stepped_program_should_finish_program_execution/1 ]).
 
 all() -> [ empty_state_should_have_fixed_memory_size,
-           pointers_should_be_set_at_the_beginning_after_init ].
+           pointers_should_be_set_at_the_beginning_after_init,
+           after_loading_program_it_should_be_available_in_state,
+           you_should_be_able_run_program,
+           you_should_be_able_step_through_program,
+           stepping_out_of_the_program_should_not_be_a_problem,
+           running_partially_stepped_program_should_finish_program_execution ].
 
 empty_state_should_have_fixed_memory_size(_Context) ->
     State = bferl_interpreter:init(),
@@ -18,5 +30,49 @@ empty_state_should_have_fixed_memory_size(_Context) ->
 
 pointers_should_be_set_at_the_beginning_after_init(_Context) ->
     State = bferl_interpreter:init(),
+
+    ?assertEqual(undefined, State#interpreter_state.instructions),
+
     ?assertEqual(0, State#interpreter_state.instructions_pointer),
     ?assertEqual(0, State#interpreter_state.memory_pointer).
+
+after_loading_program_it_should_be_available_in_state(_Context) ->
+    State = bferl_interpreter:load(["+"], bferl_interpreter:init()),
+    ?assertEqual("+", ?TO_STRING(State#interpreter_state.instructions)),
+
+    DifferentState = bferl_interpreter:init(["-"]),
+    ?assertEqual("-", ?TO_STRING(DifferentState#interpreter_state.instructions)).
+
+you_should_be_able_run_program(_Context) ->
+    State = bferl_interpreter:init(["+", "+", "+"]),
+    Output = bferl_interpreter:run(State),
+
+    ?assertEqual(3, Output#interpreter_state.instructions_pointer),
+    ?assertEqual(3, bferl_interpreter:get_memory_cell(0, Output)).
+
+you_should_be_able_step_through_program(_Context) ->
+    State = bferl_interpreter:init(["+", "+", "+"]),
+    OutputAfterFirstStep = bferl_interpreter:step(State),
+
+    ?assertEqual(1, OutputAfterFirstStep#interpreter_state.instructions_pointer),
+    ?assertEqual(1, bferl_interpreter:get_memory_cell(0, OutputAfterFirstStep)).
+
+stepping_out_of_the_program_should_not_be_a_problem(_Context) ->
+    EmptyState = bferl_interpreter:init(),
+    no_program_loaded = bferl_interpreter:step(EmptyState),
+
+    State = bferl_interpreter:init([]),
+    end_of_program = bferl_interpreter:step(State),
+
+    StateWithNegativeInstructionsPointer = State#interpreter_state{instructions_pointer = -1},
+    end_of_program = bferl_interpreter:step(StateWithNegativeInstructionsPointer).
+
+running_partially_stepped_program_should_finish_program_execution(_Context) ->
+    State = bferl_interpreter:init(["+", "+", "+"]),
+
+    StateAfterFirstStep = bferl_interpreter:step(State),
+    ?assertEqual(1, StateAfterFirstStep#interpreter_state.instructions_pointer),
+
+    Output = bferl_interpreter:run(StateAfterFirstStep),
+io:format("~p~n", [Output]),
+    ?assertEqual(3, Output#interpreter_state.instructions_pointer).
