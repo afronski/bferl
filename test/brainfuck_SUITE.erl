1f18ad5b390c04808038e950ac5d56ef9a3850b0
Wojciech Gawronski <afronski@gmail.com>
2015-07-18 09:10:22 +0200

    Adding tests for EOF and nested loops.
    I've found a bug - looping construct doesn't work properly.

--- test/brainfuck_SUITE.erl
+++ test/brainfuck_SUITE.erl
@@ -11,7 +11,9 @@
           edge_cases_for_moving_pointer/1,
           testing_loop/1,
           testing_input_and_output/1,
-          testing_hello_world/1 ]).
+          testing_hello_world/1,
+          testing_loop_with_input_and_output/1,
+          testing_nested_loops/1 ]).
 
 all() ->
     [ increment_value_in_the_cell, decrement_value_in_the_cell,
@@ -19,7 +21,9 @@ all() ->
       edge_cases_for_moving_pointer,
       testing_loop,
       testing_input_and_output,
-      testing_hello_world ].
+      testing_hello_world,
+      testing_loop_with_input_and_output,
+      testing_nested_loops ].
 
 increment_value_in_the_cell(_Context) ->
     State = bferl_interpreter:init(["+"]),
@@ -70,21 +74,21 @@ testing_loop(_Context) ->
 
 testing_input_and_output(_Context) ->
     State = bferl_interpreter:init([",", "+", "."]),
-    IoProcess = spawn_link(fun() -> io_handler("") end),
+    IoProcess = spawn_link(fun() -> io_handler("", [ $A ]) end),
 
     StateWithIO = bferl_interpreter:register_io(IoProcess, State),
     Output = bferl_interpreter:run(StateWithIO),
 
     Tape = get_tape(IoProcess),
 
-    ?assertEqual("AB", Tape),
+    ?assertEqual("B", Tape),
     ?assertEqual(length(Output#interpreter.instructions), Output#interpreter.instructions_counter).
 
 testing_hello_world(_Context) ->
     Program = bferl_tokenizer:from_file("../../../../test/assets/hello_world.bf"),
     State = bferl_interpreter:init(Program),
 
-    IoProcess = spawn_link(fun() -> io_handler("") end),
+    IoProcess = spawn_link(fun() -> io_handler("", "") end),
     StateWithIO = bferl_interpreter:register_io(IoProcess, State),
 
     bferl_interpreter:run(StateWithIO),
@@ -92,22 +96,69 @@ testing_hello_world(_Context) ->
 
     ?assertEqual("Hello World!\n", Tape).
 
+testing_loop_with_input_and_output(_Context) ->
+    State = bferl_interpreter:init([",", "[", ".", ",", "]"]),
+    IoProcess = spawn_link(fun() -> io_handler("", [$A, $B, $C, eof]) end),
+
+    StateWithIO = bferl_interpreter:register_io(IoProcess, State),
+    Output = bferl_interpreter:run(StateWithIO),
+
+    Tape = get_tape(IoProcess),
+
+    ?assertEqual("ABC", Tape),
+
+    % Calculation: 1x ',', 3x '[.,]' and +1 for `end_of_program`.
+    ?assertEqual(1 + 3 * 4 + 1, Output#interpreter.instructions_counter).
+
+testing_nested_loops(_Context) ->
+    State = bferl_interpreter:init(["[", "[", "-", "]", ">", "]"]),
+
+    State0 = State#interpreter{memory = array:set(0, 5, State#interpreter.memory)},
+    State1 = State0#interpreter{memory = array:set(1, 4, State0#interpreter.memory)},
+    State2 = State1#interpreter{memory = array:set(2, 3, State1#interpreter.memory)},
+    State3 = State2#interpreter{memory = array:set(3, 2, State2#interpreter.memory)},
+    StateWithModifiedMemory = State3#interpreter{memory = array:set(4, 1, State3#interpreter.memory)},
+
+    ?assertEqual(5, bferl_interpreter:get_memory_cell(0, StateWithModifiedMemory)),
+    ?assertEqual(4, bferl_interpreter:get_memory_cell(1, StateWithModifiedMemory)),
+    ?assertEqual(3, bferl_interpreter:get_memory_cell(2, StateWithModifiedMemory)),
+    ?assertEqual(2, bferl_interpreter:get_memory_cell(3, StateWithModifiedMemory)),
+    ?assertEqual(1, bferl_interpreter:get_memory_cell(4, StateWithModifiedMemory)),
+
+    Output = bferl_interpreter:run(StateWithModifiedMemory),
+
+    % Calculation: 5x '[>]', (5+4+3+2+1)x '[-]' and +1 for `end_of_program`.
+    ?assertEqual(20, Output#interpreter.instructions_counter),
+
+    ?assertEqual(0, bferl_interpreter:get_memory_cell(0, Output)),
+    ?assertEqual(0, bferl_interpreter:get_memory_cell(1, Output)),
+    ?assertEqual(0, bferl_interpreter:get_memory_cell(2, Output)),
+    ?assertEqual(0, bferl_interpreter:get_memory_cell(3, Output)),
+    ?assertEqual(0, bferl_interpreter:get_memory_cell(4, Output)).
+
 %% Test Helpers.
 
-io_handler(Tape) ->
-    NewTape = receive
+io_handler(Tape, Input) ->
+    {NewTape, NewInput} = receive
         {put_char, Char} ->
-            Tape ++ [Char];
+            {Tape ++ [Char], Input};
 
         {get_char, Sender} ->
-            Sender ! {received_char, $A},
-            Tape ++ [$A];
+            [ExtractedChar | Rest] = Input,
+
+            Char = case ExtractedChar of
+              eof -> 0;
+              _   -> ExtractedChar
+            end,
+
+            Sender ! {received_char, Char},
+            {Tape, Rest};
 
         {get_tape, Sender} ->
             Sender ! {tape, Tape},
-            Tape
+            {Tape, Input}
     end,
-    io_handler(NewTape).
+    io_handler(NewTape, NewInput).
 
 get_tape(IoProcess) ->
     IoProcess ! {get_tape, self()},
