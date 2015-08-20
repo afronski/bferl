39f0d287a61c937a7279f86401f667a087bcf107
Wojciech Gawronski <afronski@gmail.com>
2015-08-20 18:23:28 +0200

    Adjusting internal state as well.

--- include/interpreter_definitions.hrl
+++ include/interpreter_definitions.hrl
@@ -4,6 +4,6 @@
 -define(MEMORY_SIZE, 30000).
 -define(EMPTY_MEMORY, array:new([ {size, ?MEMORY_SIZE}, {fixed, true}, {default, 0} ])).
 
--record(interpreter, { io = {undefined, undefined}, stack = [], instructions_counter = 0,
+-record(interpreter, { io = {undefined, undefined, undefined}, stack = [], instructions_counter = 0,
                        instructions = undefined, instructions_pointer = 1,
                        memory_pointer = 0, memory = ?EMPTY_MEMORY }).
