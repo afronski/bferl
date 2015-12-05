424894af26c0642433eccca1c2303dcfd651251a
Wojciech Gawronski <afronski@gmail.com>
2015-12-05 23:14:44 +0100

    Adding new class and test (skeleton).

---
+++ src/bferl_vm_ir_executor.erl
@@ -0,0 +1,1 @@
+-module(bferl_vm_ir_executor).
---
+++ test/virtual_machine_IR_executor_SUITE.erl
@@ -0,0 +1,10 @@
+-module(virtual_machine_IR_executor_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-export([ all/0 ]).
+-export([ ]).
+
+all() ->
+    [  ].
